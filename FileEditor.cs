89ae6d1837bc13e5a80289e3a9f6c3c44adde609
Unknown <ward.v.larock@gmail.com>
2021-02-14 17:18:53 +0100

    save division, save player, fill listbox

---
+++ FileEditor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKApp
+{
+    class FileEditor
+    {
+        static string dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "KKApp save files");
+        string playerPath = dir + @"\players.txt";
+        string divisionsPath = dir + @"\divisions.txt";
+
+        public void CreateFiles()
+        {
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            if (!File.Exists(playerPath))
+                File.CreateText(playerPath);
+
+            if (!File.Exists(divisionsPath))
+                File.CreateText(divisionsPath);
+        }
+
+        public void SaveDivision(string divisionName)
+        {
+            using (StreamWriter sw = File.AppendText(divisionsPath))
+            {
+                sw.WriteLine(divisionName);
+            }
+        }
+
+        public void SavePlayer(string playerName, string divisionName)
+        {
+            using(StreamWriter sw = File.AppendText(playerPath))
+            {
+                sw.WriteLine(playerName + "," + divisionName + ",0,0");
+            }
+        }
+
+        public void SaveScore(string player1, string player2, bool player1Wins, int gamesWon)
+        {
+            
+        }
+
+        public string[] ReadDivisions()
+        {
+            if (File.ReadAllLines(divisionsPath) == null)
+                return new string[0];
+            else
+                return File.ReadAllLines(divisionsPath);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,9 +21,51 @@ namespace KKApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        FileEditor fe = new FileEditor();
         public MainWindow()
         {
             InitializeComponent();
+
+            fe.CreateFiles();
+            fillDivisionList();
+        }
+
+        private void btnAddDivision_Click(object sender, RoutedEventArgs e)
+        {
+            fe.SaveDivision(txtDivisionName.Text);
+            lstDivisions.Items.Clear();
+            fillDivisionList();
+            txtDivisionName.Text = "";
+        }
+
+        private void btnAddPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            fe.SavePlayer(txtName.Text, lblDivisionPlayer.Content.ToString());
+        }
+
+        private void btnAddScore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCopy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void fillDivisionList()
+        {
+            string[] divisions = fe.ReadDivisions();
+            for(int i = 0; i < divisions.Length; i++)
+            {
+                lstDivisions.Items.Add(divisions[i]);
+            }
+        }
+
+        private void lstDivisions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lblDivisionScore.Content = e.AddedItems[0];
+            lblDivisionPlayer.Content = e.AddedItems[0];
         }
     }
 }
